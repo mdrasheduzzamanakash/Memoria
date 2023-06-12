b433e1c8036aecb45eb2b1a517cbe248f0e46dbf
rasheduzzaman <rasheduzzaman@brainstation-23.com>
2023-06-12 13:21:24 +0600

    adds navbar content

--- MemoriaMVC/wwwroot/js/navbar.js
+++ MemoriaMVC/wwwroot/js/navbar.js
@@ -1,5 +1,7 @@
 ﻿$(function () {
     const logoutElement = document.getElementById('logout-cog');
+    const myProfileButton = document.getElementById('my-profile-cog');
+
     logoutElement.addEventListener('click', function () {
         // TODO: remove refresh token
         // TODO: invalidate jwt token or revoke it
@@ -7,5 +9,12 @@
         // temoporary implementation 
         document.cookie = "jwt" + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
         window.location.href = '/LandingPage/Index';
-    })
- })
+    });
+
+    myProfileButton.addEventListener('click', function () {
+        const loggedInUserId = localStorage.getItem('loggedInUserId');
+        window.location.href = '/Users/Profile/' + loggedInUserId;
+    });
+
+
+})
